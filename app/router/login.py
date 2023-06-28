11fb41ded8252e5d275403bc23de07e23308d05b
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2023-06-28 09:22:37 +0900

    remove unnecessary async

--- app/router/login.py
+++ app/router/login.py
@@ -6,5 +6,5 @@ import domain.service.login as login_service
 router = APIRouter()
 
 @router.post("/login", response_model=login_schema.LoginResult)
-async def login(login_info: login_schema.LoginInfo):
-    return await login_service.login(login_info)
+def login(login_info: login_schema.LoginInfo):
+    return login_service.login(login_info)
